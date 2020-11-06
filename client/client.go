d7f58e547331939a4d96b84af88fc5ffc960d333
rachel-lrr <r.lrr3247@gmail.com>
2020-11-06 10:09:04 +0800

    own code update

--- client/client.go
+++ client/client.go
@@ -1,33 +1,30 @@
 package main
 
 import (
-	"bufio"
 	"flag"
-	"fmt"
 	"net"
+	"fmt"
+	"bufio"
 	"os"
 )
 
 func read(conn *net.Conn) {
 	//TODO In a continuous loop, read a message from the server and display it.
 	reader := bufio.NewReader(*conn)
-	for {
-		line, _ := reader.ReadString('\n')
-		fmt.Println(":: ", line)
+	for{
+		msg, _ := reader.ReadString('\n')
+		fmt.Println("client received message:")
+		fmt.Println(msg)
 	}
 }
 
 func write(conn *net.Conn) {
 	//TODO Continually get input from the user and send messages to the server.
-	reader := bufio.NewReader(os.Stdin)
-	for {
+	stdin := bufio.NewReader(os.Stdin)
+	for{
 		fmt.Println("Enter text:")
-		text, _ := reader.ReadString('\n')
-		if text == "/quit\n" {
-			break
-		} else {
-			fmt.Fprintf(*conn, text)
-		}
+		text, _ := stdin.ReadString('\n')
+		fmt.Fprintln(*conn, text)
 	}
 }
 
@@ -35,12 +32,11 @@ func main() {
 	// Get the server address and port from the commandline arguments.
 	addrPtr := flag.String("ip", "127.0.0.1:8030", "IP:port string to connect to")
 	flag.Parse()
-
 	//TODO Try to connect to the server
-	conn, _ := net.Dial("tcp", *addrPtr)
-
-	go read(&conn)
-	write(&conn)
+	conn, _ := net.Dial("tcp",*addrPtr)
 	//TODO Start asynchronously reading and displaying messages
+	go read(&conn)
 	//TODO Start getting and sending user messages.
+	write(&conn)
+
 }
